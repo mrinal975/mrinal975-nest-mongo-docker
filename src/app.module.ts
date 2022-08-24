957ca46e38e8e5844ab9556e481ef940320b4772
mrinal975 <mrinalmallik1@gmail.com>
2022-08-24 17:47:57 +0600

    member resource created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MemberModule } from './member/member.module';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(
       //Replace this line with the one Cluster > Connect > Connect your Application
-      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.suflu.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
+      `mongodb://localhost:27017`,
     ),
+    MemberModule,
   ],
   controllers: [AppController],
   providers: [AppService],
