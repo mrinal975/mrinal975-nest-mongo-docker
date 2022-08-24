0c5e5baaf1d47dfd51e92b28ef1a35c6c307000d
mrinal <mrinalmallik1@gmail.com>
2022-08-24 15:54:27 +0600

    user service created
    Signed-off-by: mrinal <mrinalmallik1@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,20 @@
-FROM node:16
+# Base image
+FROM node:16-alpine
 
-RUN npm i -g @nestjs/cli
+# Create app directory
+WORKDIR /usr/src/app
 
-COPY package.json .
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
 
+# Install app dependencies
 RUN npm install
 
+# Bundle app source
 COPY . .
 
-EXPOSE 3000
+# Creates a "dist" folder with the production build
+RUN npm run build
 
-CMD ["nest", "start"]
+# Start the server using the production build
+CMD [ "node", "dist/main.js" ]
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,12 @@
-import {
-    Body, Controller, Get, Post
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-  
-  @Controller('users')
-  export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
-  
-    @Post()
-    async createOneUser(
-      @Body('name') name: string,
-      @Body('email') email: string,
-      @Body('password') password: string,
-    ) {
-      const generatedId = await this.usersService.createOneUser(
-        name,
-        email,
-        password,
-      );
-      return { id: generatedId };
-    }
-  
-    @Get()
-    getAllUsers() {
-      return this.usersService.getAllUsers();
-    }
-  
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+}
