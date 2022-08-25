5ab9550cf28772f8fbe683568101c3e0b0f30f1f
mrinal975 <mrinalmallik1@gmail.com>
2022-08-25 15:28:50 +0600

    blog controler created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { BlogController } from './blog/blog.controller';
 
 @Module({
   imports: [
     UsersModule,
     MongooseModule.forRoot(
       //Replace this line with the one Cluster > Connect > Connect your Application
-      `mongodb://localhost:27017`,
+      `mongodb://localhost:27017/test_db`,
     ),
   ],
-  controllers: [AppController],
+  controllers: [AppController, BlogController],
   providers: [AppService],
 })
 export class AppModule {}
