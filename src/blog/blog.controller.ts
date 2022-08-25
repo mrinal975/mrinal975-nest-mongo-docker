1e1af88f9d3d9fd2e85820d5adb416209939da2a
mrinal975 <mrinalmallik1@gmail.com>
2022-08-25 17:21:58 +0600

    blog added

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { BlogService } from './blog.service';
+import { CreatePostDTO } from './dto/create-post.dto';
 
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  constructor(private blogService: BlogService) {}
+
+  @Get('post')
+  async getPost(@Res() res) {
+    const posts = await this.blogService.getAll();
+    return posts;
+  }
+
+  @Post('post')
+  async addPost(@Res() res, @Body() createPostDTO: CreatePostDTO) {
+    const newPost = await this.blogService.addPost(createPostDTO);
+    return newPost;
+  }
+
+  @Get('post/:postID')
+  async findPost(@Param('postID') postID) {
+    const postData = await this.blogService.getPost(postID);
+    return postData;
+  }
+}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreatePostDTO } from './dto/create-post.dto';
 import { Post } from './interface/post.interface';
 @Injectable()
 export class BlogService {
-  constructor(@Injectable('Post') private readonly postModel: Model<Post>) {}
+  constructor(@InjectModel('Post') private readonly postModel: Model<Post>) {}
 
   async getAll(): Promise<Post[]> {
     const post = await this.postModel.find().exec();
@@ -14,6 +15,11 @@ export class BlogService {
   async addPost(createPostDTO: CreatePostDTO): Promise<Post> {
     const newPost = await new this.postModel(createPostDTO);
     return newPost.save();
+  }
+
+  async getPost(postID): Promise<Post> {
+    const ediPost = await this.postModel.findById(postID);
+    return ediPost;
   }
 
   async editPost(postID, createPostDto: CreatePostDTO): Promise<Post> {
